34ef5d5cd6848794b2ed065462811f40da155148
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-06 20:16:33 -0800

    Installing react router and declaring first routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Header from "./Header";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  // root router for entire app
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" render={() => <About title="About" />} />
+      <Route exact path="/teachers" component={Teachers} />
+      <Route exact path="/courses" component={Courses} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
