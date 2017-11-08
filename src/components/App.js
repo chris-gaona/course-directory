28bc85a120b63b4e2663fcd27a4debeb5974325b
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-07 19:27:31 -0800

    Displays 404 error route using Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from "./Home";
 import About from "./About";
 import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
+import NotFound from "./NotFound";
 
 const App = () => (
   // root router for entire app
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" render={() => <About title="About" />} />
-      <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
+      {/*Switch will look for a matching route*/}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" render={() => <About title="About" />} />
+        <Route path="/teachers" component={Teachers} />
+        <Route path="/courses" component={Courses} />
+        {/*not found*/}
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
